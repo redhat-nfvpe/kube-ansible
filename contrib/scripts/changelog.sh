35090f565a96892593c4cae90b428f0fe62c2422
Leif Madsen <lmadsen@redhat.com>
2017-12-07 18:59:21 -0500

    Changes related to release v0.1.7

--- contrib/scripts/changelog.sh
+++ contrib/scripts/changelog.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "# Release $2" > /tmp/changelog.top
-git log v$1...v$2 --pretty=format:'* [view commit](http://github.com/redhat-nfvpe/kube-centos-ansible/commit/%H) -- %s ' --reverse | grep -v "Merge" >> /tmp/changelog.top
+git log $1...$2 --pretty=format:'* [view commit](http://github.com/redhat-nfvpe/kube-centos-ansible/commit/%H) -- %s ' --reverse | grep -v "Merge" >> /tmp/changelog.top
 echo -e "\n\n" >> /tmp/changelog.top
 
 cat CHANGELOG.md >> /tmp/changelog.top
