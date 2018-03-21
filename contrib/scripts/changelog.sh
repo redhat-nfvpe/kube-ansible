ab033082049276ef19c1915127f85bcbcc94e837
Leif <lmadsen@redhat.com>
2018-03-21 08:51:33 -0400

    [release_mgmt] Update changelog script to script trailing spaces

--- contrib/scripts/changelog.sh
+++ contrib/scripts/changelog.sh
@@ -3,6 +3,7 @@
 echo "# Release $2" > /tmp/changelog.top
 git log $1...$2 --pretty=format:'* [view commit](http://github.com/redhat-nfvpe/kube-ansible/commit/%H) -- %s ' --reverse | grep -v "Merge" >> /tmp/changelog.top
 echo -e "\n\n" >> /tmp/changelog.top
+sed -i 's/[[:space:]]*$//' /tmp/changelog.top
 
 cat CHANGELOG.md >> /tmp/changelog.top
 mv /tmp/changelog.top CHANGELOG.md
